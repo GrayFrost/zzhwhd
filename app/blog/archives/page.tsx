4b7a93e5467ece129321cc83ef606722f73c0803
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2025-02-10 16:49:54 +0800

    文章归档

--- app/blog/archives/page.tsx
+++ app/blog/archives/page.tsx
@@ -1,4 +1,43 @@
+import { allPosts, Post } from "contentlayer/generated";
+import Link from "next/link";
+
+interface Archive {
+  year: string;
+  posts: Post[];
+}
+
+function getArchiveList(): Archive[] {
+  const archiveList: Archive[] = [];
+  for (const post of allPosts) {
+    const year = post.date.slice(0, 4);
+    const archive = archiveList.find((archive) => archive.year === year);
+    if (archive) {
+      archive.posts.push(post);
+    } else {
+      archiveList.push({ year, posts: [post] });
+    }
+  }
+  return archiveList.sort((a, b) => b.year.localeCompare(a.year));
+}
+
 export default function Page() {
   // 所有文章按时间排序，同时按年份拆分
-  return <div className="container max-w-3xl mx-auto p-4 md:p-6">文章归档</div>
+  const archiveList = getArchiveList();
+  return <div className="container max-w-3xl mx-auto p-4 md:p-6">
+    文章归档
+    <div>
+      {archiveList.map((archive) => (
+        <div key={archive.year}>
+          <h2 className="text-xl font-bold my-4">{archive.year}</h2>
+          <ul className="list-disc pl-6">
+            {archive.posts.map((post) => (
+              <li key={post.url}>
+                <Link href={post.url}>{post.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  </div>
 }
