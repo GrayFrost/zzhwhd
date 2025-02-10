48de46e473ce029c908c744b44ea80929f894c8f
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2025-02-10 16:35:09 +0800

    404样式

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,9 @@
-import Link from 'next/link'
+import Link from "next/link";
 export default function NotFound() {
   return (
-    <div>
-      <h1>404 Not Found</h1>
+    <div className="container min-h-screen flex items-center justify-center text-lg">
+      <div className="font-bold mr-2">404</div>
       <Link href="/">返回首页</Link>
     </div>
-  )
+  );
 }
