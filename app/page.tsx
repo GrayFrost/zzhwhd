9c504fdabbdf8c5c1f3d0670a589baff1dc7f913
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2025-02-10 15:50:54 +0800

    背景颜色

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function HomePage() {
   return (
     <>
       <main className="flex-1">
+        <div className="absolute inset-0 h-full w-full bg-[#fcf8fb] bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
         <div className="container mx-auto max-w-7xl pt-10 pb-[25px] md:pt-16">
           <Home />
         </div>
