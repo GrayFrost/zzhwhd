04f7a48fb9e57a35bcb1afd8a5fba5ac3ac72859
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2025-02-17 10:56:26 +0800

    样式

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
     <>
       <main className="flex-1">
         <div className="absolute inset-0 h-full w-full bg-[#fcf8fb] bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
-        <div className="container mx-auto max-w-7xl pt-10 pb-[25px] md:pt-16">
+        <div className="container mx-auto max-w-7xl py-8">
           <Home />
         </div>
       </main>
