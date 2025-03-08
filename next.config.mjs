793f2dc7c2ad2e59f4a1d281066458c4cdf469e0
GrayFrost <1760655799@qq.com>
2025-03-08 08:46:15 +0800

    chore: docker

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import createMDX from "@next/mdx";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  output: "standalone",
 };
 
 const withMDX = createMDX({
