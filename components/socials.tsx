bda5cd165059a73c2ac2ad7922f9d3547adbcf68
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2025-03-11 11:22:06 +0800

    feat: social dark

--- components/socials.tsx
+++ components/socials.tsx
@@ -10,7 +10,7 @@ export function Socials() {
     {
       label: "Github",
       borderColor: "border-[#1F2328]",
-      shadowColor: "hover:shadow-[#1F2328]",
+      shadowColor: "hover:shadow-[#1F2328] dark:hover:shadow-[#1F2328]",
       iconBgColor: "bg-[#1F2328]",
       url: "https://github.com/GrayFrost",
       icon: <GithubIcon2 />,
@@ -18,7 +18,7 @@ export function Socials() {
     {
       label: "掘金",
       borderColor: "border-[#1F80FF]",
-      shadowColor: "hover:shadow-[#1F80FF]",
+      shadowColor: "hover:shadow-[#1F80FF] dark:hover:shadow-[#1F80FF]",
       iconBgColor: "bg-[#1F80FF]",
       url: "https://juejin.cn/user/3350967171680920",
       icon: <JuejinIcon />,
@@ -26,7 +26,7 @@ export function Socials() {
     {
       label: "邮箱",
       borderColor: "border-[#EA4335]",
-      shadowColor: "hover:shadow-[#EA4335]",
+      shadowColor: "hover:shadow-[#EA4335] dark:hover:shadow-[#EA4335]",
       iconBgColor: "bg-[#EA4335]",
       url: "mailto:garyfrost4321@gmail.com",
       icon: <EmailIcon />,
@@ -34,7 +34,7 @@ export function Socials() {
     {
       label: "知乎",
       borderColor: "border-[#1771F6]",
-      shadowColor: "hover:shadow-[#1771F6]",
+      shadowColor: "hover:shadow-[#1771F6] dark:hover:shadow-[#1771F6]",
       iconBgColor: "bg-[#1771F6]",
       url: "https://www.zhihu.com/people/zzhui-92",
       icon: <ZhihuIcon />,
@@ -42,7 +42,7 @@ export function Socials() {
     {
       label: "简书",
       borderColor: "border-[#EA6F5A]",
-      shadowColor: "hover:shadow-[#EA6F5A]",
+      shadowColor: "hover:shadow-[#EA6F5A] dark:hover:shadow-[#EA6F5A]",
       iconBgColor: "bg-[#EA6F5A]",
       url: "https://www.jianshu.com/u/8e629fd1e3b0",
       icon: <JianshuIcon />,
@@ -50,7 +50,7 @@ export function Socials() {
   ]);
 
   const socialClass =
-    "w-full cursor-pointer rounded-md border bg-white px-2 py-1.5 transition-all hover:bg-transparent hover:shadow-[4px_4px_0px_0px] hover:shadow-gray-800 dark:bg-card dark:hover:bg-accent dark:hover:shadow-[4px_4px_0px_0px] dark:hover:shadow-accent flex items-center";
+    "w-full cursor-pointer rounded-md border bg-white dark:bg-[#1e1f26] px-2 py-1.5 transition-all hover:bg-transparent hover:shadow-[4px_4px_0px_0px]  dark:hover:shadow-[4px_4px_0px_0px] flex items-center";
 
   return (
     <>
