b0d55fab5a73401da0cc3656877c6a3eda9fd7f3
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2025-03-12 15:56:26 +0800

    fix: anchor id

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -2,34 +2,34 @@ import { MDXRemote, MDXRemoteProps } from "next-mdx-remote/rsc";
 import { options } from "@/config/mdx-options";
 import Image from "./image";
 import { createHeaderId } from "@/utils/h-id";
-import { ReactNode } from "react";
+import { ReactElement, ReactNode } from "react";
 // import Pre from "./pre";
 
 const components = {
   Image,
-  h2(props: { children: string }) {
-    return (
-      <h2
-        {...props}
-        id={createHeaderId(props.children ? `h2-${props.children}` : "")}
-      />
-    );
+  h2(props: { children: string | ReactElement<HTMLElement> }) {
+    const { children } = props;
+    const text =
+      typeof children === "string"
+        ? children
+        : children?.props?.children.toString();
+    return <h2 {...props} id={createHeaderId(text ? `h2-${text}` : "")} />;
   },
-  h3(props: { children: string }) {
-    return (
-      <h3
-        {...props}
-        id={createHeaderId(props.children ? `h3-${props.children}` : "")}
-      />
-    );
+  h3(props: { children: string | ReactElement<HTMLElement> }) {
+    const { children } = props;
+    const text =
+      typeof children === "string"
+        ? children
+        : children?.props?.children.toString();
+    return <h3 {...props} id={createHeaderId(text ? `h3-${text}` : "")} />;
   },
-  h4(props: { children: string }) {
-    return (
-      <h4
-        {...props}
-        id={createHeaderId(props.children ? `h4-${props.children}` : "")}
-      />
-    );
+  h4(props: { children: string | ReactElement<HTMLElement> }) {
+    const { children } = props;
+    const text =
+      typeof children === "string"
+        ? children
+        : children?.props?.children.toString();
+    return <h4 {...props} id={createHeaderId(text ? `h4-${text}` : "")} />;
   },
   code(props: { children: ReactNode; className?: string }) {
     const { className } = props;
