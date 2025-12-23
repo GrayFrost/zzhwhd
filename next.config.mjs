6ac59c01ce9201277bf5efa4e1c9d9bba9773d16
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2025-12-23 10:05:03 +0800

    chore: 还原部署方式

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,7 @@ import createMDX from "@next/mdx";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-  output: "export", // 启用静态导出，用于GitHub Pages部署
-  // 如果你的GitHub Pages使用子路径（如 username.github.io/repo-name），取消下面的注释并设置正确的路径
-  basePath: "/zzhwhd", // 使用username.github.io/repo-nanme形式时，需要设置正确的路径
-  // trailingSlash: true, // 可选：确保URL以斜杠结尾
-  images: {
-    unoptimized: true, // GitHub Pages不支持Next.js图片优化，需要禁用
-  },
+  output: "standalone", // 启用 standalone 输出模式，用于 Docker 部署
 };
 
 const withMDX = createMDX({
